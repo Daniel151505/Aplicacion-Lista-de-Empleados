659329d35337c207bb2a4d79100026846f73bf3b
Daniel151505 <daniel150520011@gmail.com>
2021-02-16 15:53:16 -0500

    Creando componente empleado-lista

--- Aplicacion-Lista-de-Empleados/src/app/app.module.ts
+++ Aplicacion-Lista-de-Empleados/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { EmpleadoListaComponent } from './components/empleado-lista/empleado-lista.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EmpleadoListaComponent
   ],
   imports: [
     BrowserModule,
